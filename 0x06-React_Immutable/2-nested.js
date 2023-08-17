1bb12f0c439d489affc85ce12c61021d6d73dae4
Osoroobare <brianosoro40@gmail.com>
2023-08-17 13:53:05 +0300

    Accessing nested elements

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,4 +1,4 @@
-import { fromJS } from 'immutable';
+import { fromJS } from './node_modules/immutable/dist/immutable';
 
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
